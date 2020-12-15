97c35450332a69fac11525941adb4bb7bea69c01
alessandro <finialessandro90@gmail.com>
2020-12-15 17:59:08 +0100

    end ticket detail js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,11 @@ var inputName = document.getElementById('name').value;
 var inputDistance = document.getElementById('distance').value;
 var inputAge = document.getElementById('age').value;
 var ticket = inputDistance * 0.21;
-var ticketRate = 'Tariffa Standard'
+var ticketRate = 'Tariffa Standard';
+var trainCar = Math.floor(Math.random()*10) +1;
+var cpCode = Math.floor(Math.random() * (max - min) ) + min;
+var min = 90000;
+var max = 100000;
 
 // biglietti scontati
 if (inputAge == 'under18'){
@@ -17,6 +21,8 @@ if (inputAge == 'under18'){
 // dettaglio biglietto
 document.getElementById('passenger').innerHTML = inputName;
 document.getElementById('rate').innerHTML = ticketRate;
+document.getElementById('train_car').innerHTML = trainCar;
+document.getElementById('cp_code').innerHTML = cpCode;
 document.getElementById('ticket_cost').innerHTML = ticket.toFixed(2) + '€';
 // /dettaglio biglietto
 
