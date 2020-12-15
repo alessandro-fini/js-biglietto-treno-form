3eb167e9039614ed638ecf6f2e71f6cefb13fcfb
alessandro <finialessandro90@gmail.com>
2020-12-15 15:59:47 +0100

    js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
+var inputName = document.getElementById('name').value;
+var inputDistance = document.getElementById('distance').value;
+var inputAge = document.getElementById('age').value;
 
+console.log(inputName, inputDistance, inputAge);
