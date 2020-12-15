bcdaa8ace9496ebf5ead9d88c7cb3bf84a639791
alessandro <finialessandro90@gmail.com>
2020-12-15 17:23:40 +0100

    passenger name

--- js/script.js
+++ js/script.js
@@ -2,4 +2,4 @@ var inputName = document.getElementById('name').value;
 var inputDistance = document.getElementById('distance').value;
 var inputAge = document.getElementById('age').value;
 
-console.log(inputName, inputDistance, inputAge);
+document.getElementById('passenger').innerHTML = inputName;
