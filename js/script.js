964e6ece19f9cf38f8fa6545b87caa36780b71db
alessandro <finialessandro90@gmail.com>
2020-12-15 17:47:00 +0100

    ticket detail

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 var inputName = document.getElementById('name').value;
 var inputDistance = document.getElementById('distance').value;
 var inputAge = document.getElementById('age').value;
+var ticket = inputDistance * 0.21;
+var ticketRate = 'Tariffa Standard'
 
+// biglietti scontati
+if (inputAge == 'under18'){
+  ticket -= ticket * 0.2;
+  ticketRate = 'Sconto Minorenne'
+} else if (inputAge == 'over65'){
+  ticket -= ticket * 0.4;
+  ticketRate = 'Sconto Silver'
+}
+// /biglietti scontati
+
+// dettaglio biglietto
 document.getElementById('passenger').innerHTML = inputName;
+document.getElementById('rate').innerHTML = ticketRate;
+document.getElementById('ticket_cost').innerHTML = ticket.toFixed(2) + '€';
+// /dettaglio biglietto
+
+console.log(ticket);
